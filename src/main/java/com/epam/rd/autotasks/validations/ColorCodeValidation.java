173593c5c0b7c0e02686e2bf26f1355ee78db28e
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-10-18 15:58:47 +0300

    task completed

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -5,9 +5,12 @@ import java.util.regex.Pattern;
 
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
-
-        // Put your code here
-
+        if(color == null){
+            return false;
+        }
+        Pattern  hexPattern = Pattern.compile("#[\\dA-Fa-f]{3}([\\dA-Fa-f]{3})?");
+        Matcher matcher = hexPattern.matcher(color);
+        return matcher.matches();
     }
 }
 
